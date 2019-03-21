e9fd3d85acdc161e01856f8c1ffe60d719fd3ac6
unknown <whdgus4892@gmail.com>
2019-03-21 16:14:38 +0900

    baekshin

---
+++ 1week/baekshin/createArr.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean = 0 #평균
+std = 5 #표준편차
+arr1 = np.random.normal(mean, std, [5, 5]) #정규분포
+arr2 = np.random.exponential(2, [5, 5]) #지수분포
+arr3 = arr1 * arr2
+print(arr3)
+np.save("./data", arr3) # data.npy로 저장
+
+arr = np.load("./data.npy") # 불러오기
+devArr = np.vsplit(arr, 5) # 가로로 자르기
+print(devArr[0][0])
+plt.plot(devArr[0][0]) # 그래프그리기
+plt.show()
