ba51734d0ccab907a0c327a25a5ddc9daab078e3
DevHyung <khuphj@gmail.com>
2019-03-31 18:54:25 +0900

    중간차 폴더정리

---
+++ 1week/백종현, 신동욱_01_numpy.py
@@ -0,0 +1,24 @@
+__author__ = ["nawook96(신동욱)", "JHyunB(백종현)"]
+'''
+    분포를 통해 랜덤 배열을 만들고 계산 후 파일 입출력.
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = np.array([[1,2],[3,4]])
+print(np.vsplit(t,2))#
+exit(-1)
+mean = 0 #평균
+std = 5 #표준편차
+arr1 = np.random.normal(mean, std, [5, 5]) #정규분포
+arr2 = np.random.exponential(2, [5, 5]) #지수분포
+arr3 = arr1 * arr2
+print(arr3)
+np.save("./data", arr3) # data.npy로 저장
+
+arr = np.load("./data.npy") # 불러오기
+devArr = np.vsplit(arr, 5) # 가로로 자르기
+print(devArr[0][0])
+plt.plot(devArr[0][0]) # 그래프그리기
+plt.show()
